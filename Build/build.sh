c8c1320e50f80217e091827c492b3cfa1f11d7e5
nelegatti <nelegatti.93@gmail.com>
2021-02-09 18:52:57 -0300

    first commit

---
+++ Build/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This fetches the k6-plugin-sql from GitHub and
+# builds it in your $GOPATH/src/github.com/mostafa/k6-plugin-sql
+# -ldflags="-s -w" -o sql.so github.com/mostafa/k6-plugin-sql
+
+go build -buildmode=plugin 
---
+++ query.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/godror/godror"
+)
+
+func Query() {
+	currentTime := time.Now()
+	fmt.Println("Starting at : ", currentTime.Format("03:04:05:06 PM"))
+
+	fmt.Println("... Setting up Database Connection")
+
+	db, err := sql.Open("godror", `user="dummy" password="dummy" connectString="localhost:1521/ORCLCDB.localdomain"
+    libDir="C:/instantclient_19_9"`)
+	if err != nil {
+		fmt.Println("... DB Setup Failed")
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	fmt.Println("... Opening Database Connection")
+	if err = db.Ping(); err != nil {
+		fmt.Println("Error connecting to the database: %s\n", err)
+		return
+	}
+	fmt.Println("... Connected to Database")
+
+	dbQuery := "select table_name from user_tables where table_name not like 'DM$%' and table_name not like 'ODMR$%'"
+	rows, err := db.Query(dbQuery)
+	if err != nil {
+		fmt.Println(".....Error processing query")
+		fmt.Println(err)
+		return
+	}
+	defer rows.Close()
+
+	fmt.Println("... Parsing query results")
+	var tableName string
+	for rows.Next() {
+		rows.Scan(&tableName)
+		fmt.Println(tableName)
+	}
+
+	fmt.Println("... Closing connection")
+	finishTime := time.Now()
+	fmt.Println("Finished at ", finishTime.Format("03:04:05:06 PM"))
+}
