91aed319b91d4a06bb3d9dbb1ae2b175b6e2240e
nelegatti <nelegatti.93@gmail.com>
2021-02-10 14:28:13 -0300

    update messages

--- query.go
+++ query.go
@@ -33,27 +33,27 @@ func (*QUERY) Query() {
 	defer db.Close()
 
 	fmt.Println("... Opening Database Connection")
-	if err = db.Ping(); err != nil {
-		fmt.Println("Error connecting to the database: %s\n", err)
-		return
-	}
+	// if err = db.Ping(); err != nil {
+	// 	fmt.Println("Error connecting to the database: %s\n", err)
+	// 	return
+	// }
 	fmt.Println("... Connected to Database")
 
-	dbQuery := "select table_name from user_tables where table_name not like 'DM$%' and table_name not like 'ODMR$%'"
-	rows, err := db.Query(dbQuery)
-	if err != nil {
-		fmt.Println(".....Error processing query")
-		fmt.Println(err)
-		return
-	}
-	defer rows.Close()
+	// dbQuery := "select table_name from user_tables where table_name not like 'DM$%' and table_name not like 'ODMR$%'"
+	// rows, err := db.Query(dbQuery)
+	// if err != nil {
+	// 	fmt.Println(".....Error processing query")
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer rows.Close()
 
-	fmt.Println("... Parsing query results")
-	var tableName string
-	for rows.Next() {
-		rows.Scan(&tableName)
-		fmt.Println(tableName)
-	}
+	// fmt.Println("... Parsing query results")
+	// var tableName string
+	// for rows.Next() {
+	// 	rows.Scan(&tableName)
+	// 	fmt.Println(tableName)
+	// }
 
 	fmt.Println("... Closing connection")
 	finishTime := time.Now()
